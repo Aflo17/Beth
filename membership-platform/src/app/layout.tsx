2b4ca0c148a01af29374b620f5588c46053a1187
Adonis Flores <156495766+Aflo17@users.noreply.github.com>
2025-08-09 23:35:56 -0400

    Added favicon.ico

--- membership-platform/src/app/layout.tsx
+++ membership-platform/src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>💪</text></svg>" />
         <meta httpEquiv="Content-Security-Policy" content="
           default-src 'self';
           script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com;
